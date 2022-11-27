108fac8d64bebd8a31e8624bba237cd61098983e
Marcel Mravec <marcel.mravec@gmail.com>
2022-11-27 16:54:20 +0100

    Initial Commit

---
+++ GeichaProject/GeichaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeichaProjectApp.swift
+//  GeichaProject
+//
+//  Created by Marcel Mravec on 27.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GeichaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
