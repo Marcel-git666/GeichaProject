2d5cf461419115851d8a2c128ae8b87537378e73
Marcel Mravec <marcel.mravec@gmail.com>
2022-11-27 17:10:53 +0100

    MainView correctly showing

--- GeichaProject/GeichaProjectApp.swift
+++ GeichaProject/GeichaProjectApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct GeichaProjectApp: App {
+    @StateObject var order = Order()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
+                .environmentObject(order)
         }
     }
 }
